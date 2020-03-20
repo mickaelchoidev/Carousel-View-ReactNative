6bf220e050ab69705d5a5e83057df0327249303a
Mickaelchoipro <mickaelchoipro@gmail.com>
2020-03-20 09:06:32 +0100

    error when scrolling carousel

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Home from './screen/Home'
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App
+
